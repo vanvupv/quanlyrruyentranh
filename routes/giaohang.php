a23cd52d69e9ef4843225daed4ad53f0654e2fb8
Youngboy55 <phamvanvu411.nd@gmail.com>
2024-10-12 11:37:57 +0700

    Chức năng Giao hàng - Mã khuyến mại -- DACN

---
+++ routes/giaohang.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\khuyenmai\KhuyenmaiController;
+
+Route::group(['prefix' => 'admin/khuyenmai'],
+
+    function () {
+        // Coupon
+        Route::get('/', [KhuyenmaiController::class,'index'])->name('khuyenmai');
+
+        // Coupon Add
+        Route::get('/create', [KhuyenmaiController::class,'create'])->name('khuyenmai.add');
+        Route::post('/create/store', [KhuyenmaiController::class,'store'])->name('khuyenmai.store');
+
+        // Coupon Edit
+        Route::get('/edit/{id}', [KhuyenmaiController::class,'edit'])->name('khuyenmai.edit');
+        Route::post('/edit/{id}', [KhuyenmaiController::class,'postedit'])->name('khuyenmai.postedit');
+
+    }
+);
